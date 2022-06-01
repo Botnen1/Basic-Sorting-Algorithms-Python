0b45a2611f51cb46a6d7f73e2ac5fc127f32a228
Botnen <94934743+Botnen1@users.noreply.github.com>
2022-06-01 11:40:16 +0200

    Create Selection_Sort_Algorithm.py

---
+++ Selection_Sort_Algorithm.py
@@ -0,0 +1,20 @@
+#Selection sort algorithm
+
+def selectionsort(list_b):
+    indexing_length = range(0, len(list_b) - 1)
+
+    for i in indexing_length:
+        min_value = i
+
+        for j in range(i + 1, len(list_b)):
+            if list_b[j] < list_b[min_value]:
+                min_value = j
+
+        if min_value != i:
+            list_b[min_value], list_b[i] = list_b[i], list_b[min_value]
+
+    return list_b
+
+
+print(selectionsort([4, 8, 3, 2, 7]))
+#   [2, 3, 4, 7, 8]
