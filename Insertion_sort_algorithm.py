90fb14e527a9a41791ee435e27dfe8a70298eb7c
Botnen <94934743+Botnen1@users.noreply.github.com>
2022-06-01 12:05:38 +0200

    Create Insertion_sort_algorithm.py

---
+++ Insertion_sort_algorithm.py
@@ -0,0 +1,15 @@
+#Insertion sort algorithm
+
+def insertionsort(list_c):
+    indexing_length = range(1, len(list_c))
+    for i in indexing_length:
+        value_to_sort = list_c[i]
+
+        while list_c[i-1] > value_to_sort and i > 0:
+            list_c[i], list_c[i-1] = list_c[i-1], list_c[i]
+            i = i - 1
+
+    return list_c
+
+print(insertionsort([4, 8, 3, 2, 7]))
+#   [2, 3, 4, 7, 8]
